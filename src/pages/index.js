cfc99111ef3994dba60aa26e5ba579f62cca29ac
andrparsons <andrparsons@gmail.com>
2021-07-27 17:14:05 -0400

    fixed window reference in server

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import * as React from "react";
+import { useEffect } from "react";
 import Layout from "../components/layout";
 import { navigate } from "gatsby-link";
 import Loader from "../components/loader";
 
 // markup
 const IndexPage = () => {
+  useEffect(() => {
+    navigate("/Mercury");
+  }, []);
+
   return (
     <Layout>
       <Layout.ImageContainer>
         <Loader />
       </Layout.ImageContainer>
-      {navigate("/Mercury")}
     </Layout>
   );
 };
